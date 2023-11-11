4eccd16e6da0bd9beb343f26c1825e49fbf1139f
DiscreteTom <discrete_tom@outlook.com>
2023-11-11 09:26:09 +0800

    tests: add examples/hello-world

---
+++ examples/hello-world/hello-world.test.ts
@@ -0,0 +1,18 @@
+import { TmBuilder } from "../../src";
+import * as fs from "fs";
+
+const filename = "./examples/hello-world/test.tmLanguage.json";
+
+const language = new TmBuilder({ scopeName: "source.test" })
+  .repo("comments", { name: "comment.line.test", match: /\/\//.source })
+  .append({ include: "#test" })
+  .build();
+
+const content = JSON.stringify(language, null, 2);
+
+// Usage: ts-node examples/hello-world/hello-world.test.ts
+// fs.writeFileSync(filename, content);
+
+test("hello-world", () => {
+  expect(content).toBe(fs.readFileSync(filename, "utf8"));
+});
